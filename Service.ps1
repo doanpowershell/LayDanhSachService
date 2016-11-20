6196e315dd0e95b095197fc0677f4f4ff2d41185
doanpowershell <doanpowershell@gmail.com>
2016-11-20 23:40:55 +0700

    Update and rename Service.ps1 to Service_Process.ps1

--- Service.ps1
+++
@@ -1,3 +0,0 @@
-﻿$ip=read-host "địa chỉ IP"
-$tk=read-host "Nhập TK"
-gwmi win32_service -ComputerName $ip -Credential $tk | select Name
---
+++ Service_Process.ps1
@@ -0,0 +1,7 @@
+﻿$ip=read-host "địa chỉ IP"
+$tk=read-host "Nhập TK"
+gwmi win32_service -ComputerName $ip -Credential $tk | select Name
+
+$ip=read-host "địa chỉ IP"
+$tk=read-host "Nhập TK"
+gwmi win32_process -ComputerName $ip -Credential $tk | select Name
